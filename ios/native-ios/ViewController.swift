53f6573a46f97119722f21184b0aeb072e67a9ce
Karan Rawal <kr14nov@outlook.com>
2020-02-04 12:53:26 +0530

    Added new ios application.

---
+++ ios/native-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  native-ios
+//
+//  Created by karan Rawal on 04/02/20.
+//  Copyright © 2020 karan Rawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
