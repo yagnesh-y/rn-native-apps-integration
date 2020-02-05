3e49dbef0c7e21da2087c2ed424e12b2e502b5b1
Karan Rawal <kr14nov@outlook.com>
2020-02-05 16:29:09 +0530

    Some refactoring.

--- ios/native-ios/ReactViewController.swift
+++ ios/native-ios/ReactViewController.swift
@@ -12,35 +12,43 @@ import UIKit
 
 class ReactViewController: UIViewController {
     var screen = "one";
+    var rootView: RCTRootView?;
     
     init(screen: String?) {
         if (screen != nil) {
             self.screen = screen!;
         }
         super.init(nibName: nil, bundle: nil)
+        initRootView()
     }
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+        initRootView()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    func initRootView() {
         // for dev
         let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
         // for prod
         // let jsCodeLocation = Bundle.main.url(forResource: "index", withExtension: "jsbundle")
-        let initialProps:NSDictionary = ["screen": screen] // two
+        let initialProps:NSDictionary = ["screen": self.screen] // two
         
-        // because reactnavigation has it's own navigation bar
-        self.navigationController?.navigationBar.isHidden = true
-
-        let rootView = RCTRootView(
+        rootView = RCTRootView(
             bundleURL: jsCodeLocation!,
             moduleName: "ScreenOne",
             initialProperties: initialProps as [NSObject : AnyObject],
             launchOptions: nil
         )
+    }
+    
+    override func viewDidLoad() {
         view = rootView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // because reactnavigation has it's own navigation bar
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
